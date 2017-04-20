268f3a1ea46a201d3159d78f093d863ddfbffec1
Sylvain <alonso.sylvain@outlook.com>
2017-04-20 13:56:20 +0200

    modif robot.py & adding strategic points for the path finding in map_points

---
+++ code/raspberrypi/map_points.py
@@ -0,0 +1,17 @@
+
+data ={
+    "blue": [
+        {"name":"start", "point": (9.5,16), "angle": 0},
+        {"name":"rocket", "point": (124.5,38), "angle": 270},
+        {"name":"mono1", "point": (58,50.5), "angle": 180},
+        {"name":"mono2", "point": (75.5,147), "angle": 90},
+        {"name":"remove", "point": (95,122.5), "angle": 135}]
+
+    "yellow":[
+        {"name":"start", "point": (290.5,16), "angle": 180},
+        {"name":"rocket", "point": (194.5,38), "angle": 270},
+        {"name":"mono1", "point": (58,69.5), "angle": 0},
+        {"name":"mono2", "point": (210.5,147), "angle": 90},
+        {"name":"remove", "point": (205,122.5), "angle": 45}]
+    ]
+}
